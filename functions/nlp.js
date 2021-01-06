c088e57ea57ca591f33867a400a73da225acc3d7
NawaNae <t105590029@ntut.org.tw>
2021-01-06 21:26:11 +0800

    daily prefix ok

--- functions/nlp.js
+++ functions/nlp.js
@@ -3,7 +3,8 @@ const {Functions} =require(`${Path.jsFolder}botHandler.js`);
 const fs=require("fs");
 const readline = require('readline');
 const {random}=require(`${Path.jsFolder}util`);
-const { PythonShell } = require('python-shell')
+const { PythonShell } = require('python-shell');
+const Discord = require('discord.js');
 class Scrabble extends Functions
 {
     constructor(settingsManager,botHandler)
@@ -11,7 +12,9 @@ class Scrabble extends Functions
         super(settingsManager,botHandler);
         this.name="nlp";
         this.defaultSetting.alias=[];
-        
+        this.defaultSetting.checkPeriod=1;//min
+        this.defaultSetting.sendHour=0;
+        this.defaultSetting.sendMin=0;
         this.defaultSetting.filePath= __filename.slice(__dirname.length + 1);
         this.settingsProcess();;
         this.channelSettings={}
@@ -22,10 +25,63 @@ class Scrabble extends Functions
     }
  
   
-    
+    tick()
+    {
+        var dt = new Date();
+        var h=dt.getHours();
+        var m=dt.getMinutes();
+        if(h!=this.settings.sendHour||m!=this.settings.sendMin)
+        {return;}
+        for(let ch of this.channels)
+        {
+            PythonShell.run(`${Path.pythonScriptsFolder}main.py`, {args:["每日一字"]}, (err, data) => {
+                if(err)
+                    console.log(err);
+                //const parsedString = JSON.parse(data)
+                ch.send(data);
+                //res.json(parsedString)
+              })
+            PythonShell.run(`${Path.pythonScriptsFolder}main.py`, {args:["每日一字根"]}, (err, data) => {
+                if(err)
+                    console.log(err);
+                const dicts = JSON.parse(data)
+                for(let prefix of dicts)
+                {
+                    var embed =
+                    {
+                        color:0x0099ff,
+                        author:{
+                            name:"Daily Prefix"
+                        },
+                        title:prefix.root_word,
+                        description:`${prefix.meanings}`
+
+                    }
+
+                    embed.fields=[{name:"---------",value:"examples : "}]
+                    for(let exp of prefix.examples_definitions)
+                    {
+                        let [voc,sent] = exp.split(" - ");
+                        embed.fields.push({name:voc,value:sent})
+                    }
+                    
+                }
+
+                ch.send({embed:embed});
+                //res.json(parsedString)
+              })
+        }
+    }
     onready()
     {
-
+        this.channels=[]
+        for(let id of this.settings.channels)
+        {
+            var channel=this.bot.channels.find(ch=>ch.id===id)
+            this.channels.push(channel)
+        }
+        this.tId=setInterval(()=>this.tick(),this.settings.checkPeriod*1000*60);
+        this.tick()
     }
     regularizeString(string)
     {
@@ -48,13 +104,7 @@ class Scrabble extends Functions
         if(!this.verifys(msg))return;
         
         content=this.regularizeString(msg.content);
-        PythonShell.run(`${Path.pythonScriptsFolder}main.py`, {args:["每日一字"]}, (err, data) => {
-            if(err)
-                console.log(err);
-            //const parsedString = JSON.parse(data)
-            channel.send(data);
-            //res.json(parsedString)
-          })
+        
         
     }
 }

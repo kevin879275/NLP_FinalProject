c3f40e456aae10127472dbac61d17d46fa4973ba
kevinge <gevin879275@gmail.com>
2021-01-07 20:06:56 +0800

    add linggle query function

--- NLP/main.py
+++ NLP/main.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from linggle import Linggle
 import Function.daily_voc
 import Function.daily_prefix
 import Function.rootquery
@@ -17,3 +18,8 @@ elif function == '每日一字根':
 elif function == '字根查詢':
     voc = args[0]
     print(Function.rootquery.fastfind_root(voc))
+
+elif function == 'linggle':
+    voc = args[0]
+    linggle = Linggle()
+    print(linggle.query(voc))

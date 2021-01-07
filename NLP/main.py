945478e6d25bc7381040c3d81c0b7d2ef604cdd5
kevinge <gevin879275@gmail.com>
2021-01-07 21:27:52 +0800

    update

--- NLP/main.py
+++ NLP/main.py
@@ -9,24 +9,24 @@ function = sys.argv[1]
 args = sys.argv[2:]
 
 if function == '每日一字':
-    print(json.dumps(Function.daily_voc.getDailyVoc()))
+    print(json.dumps(Function.daily_voc.getDailyVoc()), flush=True)
 
 elif function == '每日一字根':
     res = Function.daily_prefix.getDailyPrefix()
-    print(json.dumps(res))
+    print(json.dumps(res), flush=True)
 
 elif function == '字根查詢':
     voc = args[0]
-    print(json.dumps(Function.rootquery.fastfind_root(voc)))
+    print(json.dumps(Function.rootquery.fastfind_root(voc)), flush=True)
 
 elif function == '字尾查詢':
     voc = args[0]
-    print(json.dumps(Function.rootquery.fastfind_suffix(voc)))
+    print(json.dumps(Function.rootquery.fastfind_suffix(voc)), flush=True)
 
 elif function == 'linggle':
     voc = args[0]
     linggle = Linggle()
-    print(linggle.query(voc))
+    print(linggle.query(voc), flush=True)
 
 elif function == '每日一字尾':
-    print(json.dumps(Function.daily_suffix.getDailySuffix()))
+    print(json.dumps(Function.daily_suffix.getDailySuffix()), flush=True)
